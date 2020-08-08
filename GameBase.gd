24b0863d1db33c649be1d4cb61eeac7488dec8f0
LiamLombard <lplombard@hotmail.co.uk>
2020-08-08 11:02:45 +0100

    first commit

---
+++ GameBase.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _ready():
+	pass # Replace with function body.
+
+func slide_grid():
+	# Moves tile grid smoothly down
+	pass
+
+func step_grid():
+	# Moves tile grid down by one tile
+	pass
